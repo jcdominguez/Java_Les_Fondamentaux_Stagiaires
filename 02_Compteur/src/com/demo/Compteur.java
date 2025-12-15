773ce755ed7778de8b274b237fc6c377de1bdb99
jcdominguez <jcd.pro@gmail.com>
2025-12-15 11:57:18 +0100

    TP 02

---
+++ 02_Compteur/src/com/demo/Compteur.java
@@ -0,0 +1,11 @@
+package com.demo;
+
+public class Compteur {
+    public static void main(String[] args) {
+        System.out.println("Bonjour, je suis un compteur:");
+        int a = 0;
+        a++;
+        a++;
+        System.out.println(a);
+    }
+}
